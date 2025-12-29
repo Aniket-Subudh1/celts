9209be12532dad07fb527fc0aa2ba1eb40a20efa
pritee-priyadarshini <preetipriyadarshini652@gmail.com>
2025-12-29 16:34:55 +0530

    Fix

--- celts-backend/server.js
+++ celts-backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({ limit: '2mb' }));
+app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
